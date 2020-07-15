702539b24908762fd8190557de9712c575a29c49
Miguel González <mglz06@gmail.com>
2020-07-15 13:36:09 -0500

    FIX|fixed unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+
 describe('App Component', () => {
-    it('should render surely meal planner header', () => {
-        const { getByText } = render(<App />);
-        const headerElement = getByText(/surely meal planner/i);
-        expect(headerElement).toBeInTheDocument();
-    });
+  it('should render surely meal planner header', () => {
+    const { getByText } = render(<App />);
+    const headerElement = getByText(/surely meal planner/i);
+    expect(headerElement).toBeInTheDocument();
+  });
 });
--- src/modules/RecipeSearchForm/RecipeSearchForm.test.tsx
+++ src/modules/RecipeSearchForm/RecipeSearchForm.test.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import RecipeSearchForm from './RecipeSearchForm';
+import RecipeSearchForm from './RecipeSearchForm.dataLayer';
+
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
 
 describe('RecipeSearchForm Component', () => {
-    it('should render query search element', () => {
-        const { getByPlaceholderText } = render(<RecipeSearchForm />);
-        const input = getByPlaceholderText('Search');
-        expect(input).toBeInTheDocument();
-    });
-    it('should render diet multi select', () => {
-        const { getByLabelText } = render(<RecipeSearchForm />);
-        const select = getByLabelText('Diet');
-        expect(select).toBeInTheDocument();
-        expect(select).toHaveAttribute('multiple');
-    });
+  it('should render query search element', () => {
+    const { getByPlaceholderText } = render(<RecipeSearchForm />);
+    const input = getByPlaceholderText('Input a meal name, for example: eggs.');
+    expect(input).toBeInTheDocument();
+  });
+  it('should render diet select', () => {
+    const { getByText } = render(<RecipeSearchForm />);
+    const select = getByText(/select a diet/i);
+    expect(select).toBeInTheDocument();
+  });
 
-    it('should render cuisine multi select', () => {
-        const { getByLabelText } = render(<RecipeSearchForm />);
-        const select = getByLabelText('Cuisine Type');
-        expect(select).toBeInTheDocument();
-        expect(select).toHaveAttribute('multiple');
-    });
+  it('should render cuisine select', () => {
+    const { getByText } = render(<RecipeSearchForm />);
+    const select = getByText(/select a dish type/i);
+    expect(select).toBeInTheDocument();
+  });
 
-    it('should render dish type multi select', () => {
-        const { getByLabelText } = render(<RecipeSearchForm />);
-        const select = getByLabelText('Dish Type');
-        expect(select).toBeInTheDocument();
-        expect(select).toHaveAttribute('multiple');
-    });
+  it('should render dish type select', () => {
+    const { getByText } = render(<RecipeSearchForm />);
+    const select = getByText(/select a cuisine type/i);
+    expect(select).toBeInTheDocument();
+  });
 });
